12f66c5a8957f41357607d7fa17d22f82da5254f
Yuin0329 <95572957+Yuin0329@users.noreply.github.com>
2022-11-25 04:22:15 +0800

    Add files via upload

--- 期中第一題.py
+++ 期中第一題.py
@@ -5,28 +5,33 @@ Created on Sun Nov 20 11:36:45 2022
 @author: Yuin
 """
 
-def snake_and_camel(text):   
-    if text.find('_')!=-1 and len(text)<=20:
-      a=text.split('_')
-      camel = a[0] + ''.join(n.title() for n in a[1:])
-      print(camel)
-    elif text.find('_')==-1 and len(text)<=20  :
-        list1=[]
-        for n in text: 
-            if n.isupper():
-                text = text.replace(n,"_"+str(n.lower())) 
-                list1+=[text]
-        del list1[:-1]
-        k="".join(map(str,list1))
-        print(k)
-    elif  len(text)>20:
-        print('invalid name')
-    else:
-        print('') 
-N=int(input())
-list1=[]
-for i in range(0,N):
-    text=input(str())
-    list1+=[text]
-
+def snake_and_camel():
+    N=int(input())
+    list0=[]
+    list1=[]
+    for i in range(0,N):
+        text=input(str())
+        list0+=[text]
+    for item in list0:
+            if item.find('_')!=-1 and len(item)<=20:
+                a=item.split('_')
+                camel = a[0] + ''.join(n.title() for n in a[1:])
+                list1.append(camel)
+            elif item.find('_')==-1 and len(text)<=20  :
+                for n in item: 
+                    if n.isupper():
+                        item = item.replace(n,"_"+str(n.lower())) 
+                        list1+=[item]
+                        del list1[:-1]
+                        k="".join(map(str,list1))
+                        list1.append(k)
+                    elif  len(item)>20:                       
+                        list1.append('invalid name')
+            else: 
+                list1.append('')
+    print(list1)
+    k="\n".join(list1)
+    print(k)
 snake_and_camel() 
+
+
