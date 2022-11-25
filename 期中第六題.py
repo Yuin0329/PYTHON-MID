8e0f2758231954903d2533c2d4095a29559636a5
Yuin0329 <95572957+Yuin0329@users.noreply.github.com>
2022-11-26 04:37:07 +0800

    Add files via upload

--- 期中第六題.py
+++ 期中第六題.py
@@ -6,19 +6,19 @@ Created on Sat Nov 26 01:55:52 2022
 """
 n = int(input())
 for i in range(n):
-    rule = True
     a = [int(x)for x in input().split()]
     b = [int(x)for x in input().split()]
+    rule = True
     if a[1] == a[3] or a[1] != a[5] or b[1] == b[3] or b[1] != b[5]:
         print("A",end="")
         rule = False
     if a[6] != 1 or b[6] != 0:
         print("B",end="")
-        rule = False
+        rule= False
     if a[1] == b[1] or a[3] == b[3] or a[5] == b[5]:
         print("C",end="")
         rule = False
     if rule:
-        print("None")
+        print("None") 
     else:
         print()
