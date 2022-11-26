63574a69ecbf4d9d84899029aca64d0b36a442d3
Yuin0329 <95572957+Yuin0329@users.noreply.github.com>
2022-11-27 04:48:32 +0800

    Add files via upload

--- 期中第十題.py
+++ 期中第十題.py
@@ -1,15 +1,12 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Nov 26 18:43:31 2022
+Created on Sun Nov 27 04:43:02 2022
 
 @author: jayyu
 """
-
 import math
-
 def compute(p, q):
     return math.gcd(p, q)
-
 x, y = map(int,input().split("/"))
 m, n = map(int,input().split("/"))
 p = (x*n + m*y) / compute(x*n + m*y,y*n)
