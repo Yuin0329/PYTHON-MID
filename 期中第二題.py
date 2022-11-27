0385c65786f98f7a35ef7b0c135703f7e1d23ab3
Yuin0329 <95572957+Yuin0329@users.noreply.github.com>
2022-11-27 23:01:57 +0800

    Add files via upload

--- 期中第二題.py
+++ 期中第二題.py
@@ -1,20 +1,44 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Nov 27 16:22:44 2022
+Created on Sun Nov 27 20:17:50 2022
 
-@author: jayyu
+@author: Yuin
 """
 
 import sys
-list0=[]
-k=input().split()
-list0=k
-for item in list0:
-    if len(item)>=8:
+a,b,c,d=input().split()
+
+if len(str(a))>8:
         sys.exit("請重新輸入，資料長度超過長度")
-    if item.isnumeric()==True:
-        eval(item) 
-print('|%8s %8s|' %(list0[0],list0[1]))
-print('|%8s %8s|' %(list0[2],list0[3]))
-print('|%-8s %-8s|' %(list0[0],list0[1]))
-print('|%-8s %-8s|' %(list0[2],list0[3]))
+elif a.isalpha()==False:
+        print(d.find("."))
+        if a.find(".")!=-1:
+            k=float(a)
+            a=round(k,2)
+if len(str(b))>8:
+        sys.exit("請重新輸入，資料長度超過長度")
+elif b.isalpha()==False:
+        print(d.find("."))
+        if b.find(".")!=-1:
+            l=float(b)
+            b=round(l,2)
+if  len(str(c))>8:
+        sys.exit("請重新輸入，資料長度超過長度")
+elif c.isalpha()==False:
+        print(d.find("."))
+        if c.find(".")!=-1:
+            m=float(c)
+            c=round(m,2)
+if len(str(d))>8:
+        sys.exit("請重新輸入，資料長度超過長度")
+elif d.isalpha()==False:
+        print(d.find("."))
+        if d.find(".")!=-1:
+            n=float(d)
+            d=round(n,2)
+
+print('|%8s %8s|' %(a,b))
+print('|%8s %8s|' %(c,d))
+print('|%-8s %-8s|' %(a,b)) 
+print('|%-8s %-8s|' %(c,d)) 
+ 
